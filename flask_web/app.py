eb2f9ae287b2dbf8161212aeb4e8935d36a12750
ZukaCS <zukakekelidze4@gmail.com>
2023-01-08 23:06:47 +0400

    implement 'report a bug' feature

--- flask_web/app.py
+++ flask_web/app.py
@@ -8,18 +8,28 @@ from flask import (Flask, current_app, flash, jsonify, make_response, redirect,
                    render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from email.message import EmailMessage
+import ssl
+import smtplib
 from flask_session import Session
 
+#connect to database
 conn = sqlite3.connect('static/database.db', check_same_thread=False)
 print("opened successfully")
+
+#configure email
+email_sender = 'bugscountryinfo@gmail.com'
+email_password = 'zazsazegqycndlom'
+email_receiver = 'zukakekelidze4@gmail.com'
+
 
 app = Flask(__name__)
 app.secret_key = 'aaccssdd321'
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+#configure cookies
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-
 Session(app)
 
 @app.after_request
@@ -48,7 +58,7 @@ for index in DATA:
     country_index[index["name"]["common"]]=i
     countries.add(index["name"]["common"])
     i+=1
-
+countries = sorted(countries)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -96,7 +106,6 @@ def login():
             return ("Username or password is incorrect")
 
 
-
 @app.route("/logOut")
 def logout():
     session.clear()
@@ -133,6 +142,7 @@ def favourites():
             countries = set()
             for item in favourites:
                 countries.add(item[0])
+            
             print(countries)
             DATAS = []
             for country in countries:
@@ -140,13 +150,13 @@ def favourites():
                     "name":DATA[country_index[country]]["name"]["common"],
                     "flag":DATA[country_index[country]]["flags"]["png"]
                     })
-
-            return render_template("favourites.html", DATAS=DATAS)
+            favouritesLength = len(DATAS)
+            
+            return render_template("favourites.html", DATAS=DATAS, favouritesLength=favouritesLength)
 
 #add to favourites     
 @app.route("/add", methods=["POST"])
 def addFav():
-
     if request.method == "POST":
         country = request.form.get("country")
         if conn.execute("SELECT id from favourites where person_id = ? and country = ?", (session["user_id"], country)).fetchone():
@@ -164,11 +174,35 @@ def remove():
         conn.execute("DELETE FROM favourites where id in (SELECT id from favourites where person_id = ? and country = ?)", (session["user_id"], country))
         conn.commit()
         return ("removed from favourites")
-if __name__ == "__main__":
-    app.run(debug=True)
     
+@app.route("/reportpage", methods=[ "GET"])
+def reportPage():
+    if request.method == "GET":
+        return render_template("bug.html")
     
+@app.route("/reported", methods=["POST"])
+def reported():
+    if request.method == "POST":
+        subject = request.form.get("subject")
+        text = request.form.get("text")
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        if text == "" and subject == "":
+            return "empty"
+        if len(text) < 40:
+            return "text less than 40 chars" 
+        if len(subject) < 10:
+            return "subject less than 10 chars"
 
+        em['Subject'] = subject
+        em.set_content(text)
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+    return "success"
+        
     
     
     
